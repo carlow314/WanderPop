ebcb59357347f08d2d2d9b415cf758583ebe0e9c
Ryan Norrid <rnorrid@gmail.com>
2017-06-24 11:46:36 -0600

    pushing a version where the css is still in place

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+
+$( function() {
+    var availableTags = [
+      { label: "Denver, CO", category: "DIA" },
+      { label: "Austin, TX", category: "AUS" },
+      { label: "New York City, NY", category: "JFK" },
+      { label: "Los Angeles, LA", category: "LAX" },
+      { label: "Atlanta, GA", category: "ATL" },
+      { label: "Chicago, IL", category: "ORD" },
+      { label: "Baltimore, MD", category: "BWI" },
+      { label: "Boston, MA", category: "BOS" },
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+  } );
