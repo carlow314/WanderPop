457cafad00da314ed06352747f9780f685554a15
Ryan Norrid <rnorrid@gmail.com>
2017-06-27 19:06:23 -0600

    added hide and show functions for the first and second part of the search

--- javascript/app.js
+++ javascript/app.js
@@ -1,64 +1,73 @@
+$( document ).ready(function() {
+  
+  $( "#sectionB" ).hide();  
 
-$( function() {
-    var availableTags = [
-      { label: "Denver, CO - DEN", category: "DEN" },
-      { label: "Austin, TX - AUS", category: "AUS" },
-      { label: "New York City, NY - JFK", category: "JFK" },
-      { label: "Los Angeles, CA - LAX", category: "LAX" },
-      { label: "Atlanta, GA -ATL", category: "ATL" },
-      { label: "Chicago, IL - ORD", category: "ORD" },
-      { label: "Baltimore, MD - BWI", category: "BWI" },
-      { label: "Boston, MA - BOS", category: "BOS" },
-    ];
-    $( "#originCity" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  $( function() {
+      var availableTags = [
+        { label: "Denver, CO - DEN", category: "DEN" },
+        { label: "Austin, TX - AUS", category: "AUS" },
+        { label: "New York City, NY - JFK", category: "JFK" },
+        { label: "Los Angeles, CA - LAX", category: "LAX" },
+        { label: "Atlanta, GA -ATL", category: "ATL" },
+        { label: "Chicago, IL - ORD", category: "ORD" },
+        { label: "Baltimore, MD - BWI", category: "BWI" },
+        { label: "Boston, MA - BOS", category: "BOS" },
+      ];
+      $( "#originCity" ).autocomplete({
+        source: availableTags
+      });
+    } );
 
-$( function() {
-    var availableTags = [
-      { label: "Denver, CO - DEN", category: "DEN" },
-      { label: "Austin, TX - AUS", category: "AUS" },
-      { label: "New York City, NY - JFK", category: "JFK" },
-      { label: "Los Angeles, CA - LAX ", category: "LAX" },
-      { label: "Atlanta, GA - ATL", category: "ATL" },
-      { label: "Chicago, IL - ORD", category: "ORD" },
-      { label: "Baltimore, MD - BWI", category: "BWI" },
-      { label: "Boston, MA - BOS", category: "BOS" },
-    ];
-    $( "#destCity" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  $( function() {
+      var availableTags = [
+        { label: "Denver, CO - DEN", category: "DEN" },
+        { label: "Austin, TX - AUS", category: "AUS" },
+        { label: "New York City, NY - JFK", category: "JFK" },
+        { label: "Los Angeles, CA - LAX ", category: "LAX" },
+        { label: "Atlanta, GA - ATL", category: "ATL" },
+        { label: "Chicago, IL - ORD", category: "ORD" },
+        { label: "Baltimore, MD - BWI", category: "BWI" },
+        { label: "Boston, MA - BOS", category: "BOS" },
+      ];
+      $( "#destCity" ).autocomplete({
+        source: availableTags
+      });
+    } );
 
-$( function() {
-    var availableTags = [
-      { label: "Denver, CO - DEN", category: "DEN" },
-      { label: "Austin, TX - AUS", category: "AUS" },
-      { label: "New York City, NY - JFK", category: "JFK" },
-      { label: "Los Angeles, CA - LAX", category: "LAX" },
-      { label: "Atlanta, GA - ATL", category: "ATL" },
-      { label: "Chicago, IL - ORD", category: "ORD" },
-      { label: "Baltimore, MD -BWI", category: "BWI" },
-      { label: "Boston, MA - BOS", category: "BOS" },
-    ];
-    $( "#originCityB" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  $( function() {
+      var availableTags = [
+        { label: "Denver, CO - DEN", category: "DEN" },
+        { label: "Austin, TX - AUS", category: "AUS" },
+        { label: "New York City, NY - JFK", category: "JFK" },
+        { label: "Los Angeles, CA - LAX", category: "LAX" },
+        { label: "Atlanta, GA - ATL", category: "ATL" },
+        { label: "Chicago, IL - ORD", category: "ORD" },
+        { label: "Baltimore, MD -BWI", category: "BWI" },
+        { label: "Boston, MA - BOS", category: "BOS" },
+      ];
+      $( "#originCityB" ).autocomplete({
+        source: availableTags
+      });
+    } );
 
-$( function() {
-    var availableTags = [
-      { label: "Denver, CO - DEN", category: "DEN" },
-      { label: "Austin, TX - AUS", category: "AUS" },
-      { label: "New York City, NY - JFK", category: "JFK" },
-      { label: "Los Angeles, CA - LAX", category: "LAX" },
-      { label: "Atlanta, GA -ATL", category: "ATL" },
-      { label: "Chicago, IL -ORD", category: "ORD" },
-      { label: "Baltimore, MD - BWI", category: "BWI" },
-      { label: "Boston, MA -BOS", category: "BOS" },
-    ];
-    $( "#destCityB" ).autocomplete({
-      source: availableTags
-    });
-  } );
+  $( function() {
+      var availableTags = [
+        { label: "Denver, CO - DEN", category: "DEN" },
+        { label: "Austin, TX - AUS", category: "AUS" },
+        { label: "New York City, NY - JFK", category: "JFK" },
+        { label: "Los Angeles, CA - LAX", category: "LAX" },
+        { label: "Atlanta, GA -ATL", category: "ATL" },
+        { label: "Chicago, IL -ORD", category: "ORD" },
+        { label: "Baltimore, MD - BWI", category: "BWI" },
+        { label: "Boston, MA -BOS", category: "BOS" },
+      ];
+      $( "#destCityB" ).autocomplete({
+        source: availableTags
+      });
+    } );
+  $( "#go" ).click(function() {
+    $( "#sectionA" ).hide();
+    $( "#sectionB" ).show( 1000 );
+  });
+
+});
