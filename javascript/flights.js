4b982416d20814e946b66b560da95d9b109e9879
Jeff Carlow <jeff.carlow@gmail.com>
2017-06-28 21:08:01 -0600

    Auto stash before merge of "master" and "origin/master"
    changes to pull flight api data

--- javascript/flights.js
+++ javascript/flights.js
@@ -31,26 +31,71 @@ $.ajax({
      dataType: 'json',
      // The query we want from Google QPX, This will be the variable we created in the beginning
      data: JSON.stringify(FlightRequest),
-     success: function (data) {
-var tripCost = data.trips.tripOption[0].saleTotal;
-var legOneDest = data.trips.tripOption[0].slice[0].segment[0].leg[0].destination;
-var legOneDepart = data.trips.tripOption[0].slice[0].segment[0].leg[0].origin;
-var legOneDepartTime = data.trips.tripOption[0].slice[0].segment[0].leg[0].departureTime;
-var legOneArrivalTime = data.trips.tripOption[0].slice[0].segment[0].leg[0].arrivalTime;
-var legTwoDest = data.trips.tripOption[0].slice[1].segment[0].leg[0].destination;
-var legTwoDepart = data.trips.tripOption[0].slice[1].segment[0].leg[0].origin;
-var legTwoDepartTime = data.trips.tripOption[0].slice[1].segment[0].leg[0].departureTime;
-var legTwoArrivalTime = data.trips.tripOption[0].slice[1].segment[0].leg[0].arrivalTime;
+     success: function (response) {
+       console.log(response);
+       var flightOptions = response.trips.tripOption;
+       var saleTotals = [];
+       var roundtripData = [];
+       for (var i = 0; i < flightOptions.length; i += 1) {
+         var flightOption = flightOptions[i];
+         saleTotals.push(flightOption.saleTotal);
+         roundtripData.push(flightOption.slice);
+         console.log(saleTotals);
+       }
 
-console.log(data);
-console.log("Your total trip cost is:", tripCost);
-console.log("This is your destination of your first flight:",legOneDest);
-console.log("This is your origin of your first flight:",legOneDepart);
-console.log("Your first leg leaves at:",moment(legOneDepartTime).format("MMMM Do YYYY, h:mm a"));
-console.log("Your first leg arrives at:",moment(legOneArrivalTime).format("MMMM Do YYYY, h:mm a"));
-console.log("This is your destination of your second leg:",legTwoDest);
-console.log("This is your origin of your second leg:",legTwoDepart);
-console.log("Your second leg leaves at:",moment(legTwoDepartTime).format("MMMM Do YYYY, h:mm"));
-console.log("Your second leg arrives at:",moment(legTwoArrivalTime).format("MMMM Do YYYY, h:mm a"));
-  },
-    });
+       var conciseRoundTripData = [];
+       for (var i = 0; i < roundtripData.length; i +=1) {
+        var currentRoundTrip = roundtripData[i];
+        var outboundLeg = currentRoundTrip[0];
+        var inboundLeg = currentRoundTrip[1];
+        var outboundLegInfo = outboundLeg.segment[0].leg[0];
+        var outboundLegDestination = outboundLegInfo.destination;
+        var outboundLegOrigin = outboundLegInfo.origin;
+        var inboundLegInfo = inboundLeg.segment[0].leg[0];
+        var inboundLegDestination = inboundLegInfo.destination;
+        var inboundLegOrigin = inboundLegInfo.origin;
+        var outboundLegDeparture = outboundLegInfo.departureTime;
+        var outboundLegArrival = outboundLegInfo.arrivalTime;
+        var inboundLegDeparture = inboundLegInfo.departureTime;
+        var inboundLegArrival = inboundLegInfo.arrivalTime;
+        var conciseRoundTrip = {
+          outboundLeg: {
+            destination: outboundLegDestination,
+            origin: outboundLegOrigin,
+            arrival: outboundLegArrival,
+            departure: outboundLegDeparture
+          },
+          inboundLeg: {
+            destination: inboundLegDestination,
+            origin: inboundLegOrigin,
+            arrival: inboundLegArrival,
+            departure: inboundLegDeparture
+          }
+        };
+        conciseRoundTripData.push(conciseRoundTrip);
+       }
+       console.log(conciseRoundTripData);
+//        var shortenSliceOne = data.trips.tripOption[0].slice[0].segment[0].leg[0];
+//        var shortenSliceTwo = data.trips.tripOption[0].slice[1].segment[0].leg[0];
+// var tripCost = data.trips.tripOption[0].saleTotal;
+// var legOneDest = shortenSliceOne.destination;
+// var legOneDepart = shortenSliceOne.origin;
+// var legOneDepartTime = shortenSliceOne.departureTime;
+// var legOneArrivalTime = shortenSliceOne.arrivalTime;
+// var legTwoDest = shortenSliceTwo.destination;
+// var legTwoDepart = shortenSliceTwo.origin;
+// var legTwoDepartTime = shortenSliceTwo.departureTime;
+// var legTwoArrivalTime = shortenSliceTwo.arrivalTime;
+//
+// console.log(data);
+// console.log("Your total trip cost is:", tripCost);
+// console.log("This is your destination of your first flight:",legOneDest);
+// console.log("This is your origin of your first flight:",legOneDepart);
+// console.log("Your first leg leaves at:",moment(legOneDepartTime).format("MMMM Do YYYY, h:mm a"));
+// console.log("Your first leg arrives at:",moment(legOneArrivalTime).format("MMMM Do YYYY, h:mm a"));
+// console.log("This is your destination of your second leg:",legTwoDest);
+// console.log("This is your origin of your second leg:",legTwoDepart);
+// console.log("Your second leg leaves at:",moment(legTwoDepartTime).format("MMMM Do YYYY, h:mm"));
+// console.log("Your second leg arrives at:",moment(legTwoArrivalTime).format("MMMM Do YYYY, h:mm a"));
+  }
+});
